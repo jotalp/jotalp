bf34c35020fb57cda7ca43150dd3d3389ebf2b51
jotalp <holypants77@protonmail.com>
2023-05-10 05:50:41 -0400

    created search page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
+    function searchFunction() {
+        // Get the input value and convert it to lowercase
+        let input = document.getElementById("searchInput").value.toLowerCase();
 
+    // Get the list of items to be searched (in this case, a list of names)
+    let namesList = document.getElementsByClassName("names");
 
-var message = "Hello World";
+    // Loop through the list of names and hide/show them based on the search input
+    for (let i = 0; i < namesList.length; i++) {
+          if (namesList[i].innerHTML.toLowerCase().indexOf(input) > -1) {
+        namesList[i].style.display = "";
+          } else {
+        namesList[i].style.display = "none";
+          }
+        }
+      }
 
-function sendMessage() {
-    console.log(message);
-}
-
-sendMessage();
-
-console.log(message = "Hello World");
-
-
+      
